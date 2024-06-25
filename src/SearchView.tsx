a5cfcf787e70520dd8e61df0df6c946af6db8a97
Yumer A <y.yumer@gmail.com>
2024-06-25 11:36:06 +0200

    Add Search view and configuration

---
+++ src/SearchView.tsx
@@ -0,0 +1,60 @@
+import React, { useCallback, useState } from "react";
+import axios from "./axiousConfig";
+import { debounce } from "lodash";
+import SearchViewItem from "./SearchViewItem";
+
+interface Product {
+  id: number;
+  name: string;
+  category: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+}
+
+const SearchView = (): JSX.Element => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState<Product[]>([]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedSearch = useCallback(
+    debounce((query: string) => {
+      axios
+        .get(`/products/search?q=${query}`)
+        .then((response) => {
+          setSearchResults(response.data);
+        })
+        .catch((error) => {
+          console.error("Error searching products:", error);
+        });
+    }, 500),
+    []
+  ); // Debounce time in milliseconds
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value;
+    setSearchTerm(query);
+    debouncedSearch(query);
+  };
+
+  return (
+    <div>
+      <h1>Product Search</h1>
+      <input
+        type="text"
+        placeholder="Search products..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <ul>
+        {searchResults.map((product: Product) => (
+          <SearchViewItem product={product} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SearchView;
+
+export type { Product };
---
+++ src/SearchViewItem.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import type { Product } from "./SearchView";
+
+const SearchViewItem = ({ product }: { product: Product }): JSX.Element => {
+  return (
+    <li key={product.id}>
+      <Link to={""} className="product-list-item">
+        <div>{product.name}</div>
+        <div>{product.category}</div>
+        <div>{product.price} SEK</div>
+      </Link>
+    </li>
+  );
+};
+
+export default SearchViewItem;
